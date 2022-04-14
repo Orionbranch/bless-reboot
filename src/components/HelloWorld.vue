b3b3bb100430624c3a97c08a199977a70c744195
Orionbranch <rigel.ghost039@gmail.com>
2022-04-15 02:56:58 +0900

    imgを表示出来ません…

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+    <h1>OrionBranch</h1>
+    <img src="assets/img-1.png" alt="">
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+  .hello {
+    text-align: center;
+  }
+</style>
